fc0a51e96f367ee0b4e9bdbeb2a3412909b07ae2
reinhardjs <reinhardjsilalahi@gmail.com>
2024-04-05 22:51:01 +0700

    used cluster mode for nestjs

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -7,20 +7,20 @@ const numCPUs = require('os').cpus().length;
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    // if (cluster.isMaster) {
-    //     console.log(`Master ${process.pid} is running`);
+    if (cluster.isMaster) {
+        console.log(`Master ${process.pid} is running`);
 
-    //     // Fork workers.
-    //     for (let i = 0; i < numCPUs; i++) {
-    //         cluster.fork();
-    //     }
+        // Fork workers.
+        for (let i = 0; i < numCPUs; i++) {
+            cluster.fork();
+        }
 
-    //     cluster.on('exit', (worker, code, signal) => {
-    //         console.log(`worker ${worker.process.pid} died`);
-    //     });
-    // } else {
+        cluster.on('exit', (worker, code, signal) => {
+            console.log(`worker ${worker.process.pid} died`);
+        });
+    } else {
         const port = process.env.PORT || 4200;
         await app.listen(port);
-    // }
+    }
 }
 bootstrap()
