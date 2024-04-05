249d9024b675892a9394a120b856bec7bb85d7d3
reinhardjs <reinhardjsilalahi@gmail.com>
2024-04-06 04:58:07 +0700

    modified k6 scripts

--- go.js
+++ go.js
@@ -1,5 +1,4 @@
 import http from 'k6/http';
-import { group } from 'k6';
 
 // 103.172.204.152
 const host = 'localhost'
@@ -9,26 +8,20 @@ export const options = {
     scenarios: {
         scenario1: {
             executor: 'constant-vus',
-            vus: 1000,
+            vus: 200,
             duration: '10s', // Total test duration
-            gracefulStop: '30s',
-            exec: 'serial'
+            gracefulStop: '30s'
         },
     },
 };
 
-export function serial() {
-    // each user have 1 requests
-    for (let i = 0; i < 1; i++) {
-        group('API Requests', function () {
-            // Define your request headers with the JWT token
-            const headers = {
-                'Content-Type': 'application/json',
-            };
+export default function () {
+    // Define your request headers with the JWT token
+    const headers = {
+        'Content-Type': 'application/json',
+    };
 
-            // Make an authenticated request
-            const response = http.get(`http://${host}:${port}`, { headers: headers });
-            console.log(`User ${__VU}: Request ${__ITER + 1} - Status code: ${response.status}`);
-        });
-    }
+    // Make an authenticated request
+    const response = http.get(`http://${host}:${port}`, { headers: headers });
+    console.log(`User ${__VU}: Request ${__ITER + 1} - Status code: ${response.status}`);
 }
