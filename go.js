07b6a94cbfdadeb53aa723e95f5209d44dbea92b
reinhardjs <reinhardjsilalahi@gmail.com>
2024-04-06 11:33:18 +0700

    modified k6 scripts

--- go.js
+++ go.js
@@ -9,13 +9,14 @@ export const options = {
         scenario1: {
             executor: 'constant-vus',
             vus: 100,
-            duration: '3s', // Total test duration
-            gracefulStop: '30s'
+            duration: '5s', // Total test duration
+            gracefulStop: '30s',
+            exec: 'paralel',
         },
     },
 };
 
-export default async function () {
+export function serial () {
     // Define your request headers with the JWT token
     const headers = {
         'Content-Type': 'application/json',
@@ -24,4 +25,34 @@ export default async function () {
     // Make an authenticated request
     const response = http.get(`http://${host}:${port}`, { headers: headers });
     console.log(`User ${__VU}: Request ${__ITER} - Status code: ${response.status}`);
+}
+
+export function paralel() {
+    // Define your request headers with the JWT token
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+
+    // Define an array to hold the requests
+    let requests = [];
+
+    // Define the number of parallel requests
+    const numRequests = 10;
+
+    // Populate the requests array with the requests
+    for (let i = 0; i < numRequests; i++) {
+        requests.push({
+            method: 'GET',
+            url: `http://${host}:8080`,
+            headers: headers,
+        });
+    }
+
+    // Send parallel requests using batch
+    const responses = http.batch(requests);
+
+    // Print the responses
+    responses.forEach((res, index) => {
+        console.log(`Response ${index+1}: Status code: ${res.status}`);
+    });
 }
