403302d1c36e0bb87e17c3a2924851a8eed0b306
reinhardjs <reinhardjsilalahi@gmail.com>
2024-07-29 20:10:13 +0000

    ignore iterations param variable

--- nest/src/worker.ts
+++ nest/src/worker.ts
@@ -3,14 +3,14 @@ import { parentPort } from 'worker_threads';
 
 // Listen for messages from the parent thread
 parentPort?.on('message', async (task: { iterations: number }) => {
-  let counter = 0;
-
-  for (let i = 0; i < task.iterations; i++) {
-    // Mocking a time-consuming task
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    counter++;
+  // ignore iterations
+  let count = 0;
+  for (let i = 1; i <= 5000; i++) {
+    for (let j = 1; j <= 5000; j++) {
+      count += i + j;
+    }
   }
 
   // Send the result back to the parent thread
-  parentPort?.postMessage(counter);
+  parentPort?.postMessage(count);
 });
